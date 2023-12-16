6db18a61e0aaa4246be05692d17a143a63aacc93
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2023-12-16 09:48:39 +0530

    Create String's Count.java

---
+++ Easy/String's Count/String's Count.java
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+  
+        
+        int t = Integer.parseInt(br.readLine().trim()); 
+
+        while(t > 0){
+        	long n = Integer.parseInt(br.readLine().trim()); 
+            Solution ob = new Solution();
+            System.out.println(ob.countStr(n)); 
+            t--;
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    static long countStr(long n)
+	{
+	    // code here
+	    return 1+n+n+n*(n-1)+n*(n-1)/2 + n*(n-1)*(n-2)/2;
+	}
+} 
