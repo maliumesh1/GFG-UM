23acae650aaf20edacc38b4932928cfa43b6c51b
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2023-12-19 09:34:05 +0530

    Create Rightmost different bit.java

---
+++ Easy/Rightmost different bit/Rightmost different bit.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+class Main{
+    
+    
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner sc=new Scanner(System.in);
+		
+		//input number of testcases
+		int t=sc.nextInt();
+		int m,n;
+		while(t-->0) {
+		    
+		    
+            Solution obj = new Solution();
+            //input m and n
+		    m = sc.nextInt();
+		    n = sc.nextInt();
+		    System.out.println(obj.posOfRightMostDiffBit(m, n));
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find the first position with different bits.
+    public static int posOfRightMostDiffBit(int m, int n)
+    {
+            
+        // Your code here        
+            for(int i=0;i<32;i++){
+            if((m & (1<<i))!=(n&(1<<i))){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
+
+
+                           
