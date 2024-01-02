f9077bf437066580ff6d95dfd338ef724e5d8563
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-01-02 21:24:07 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Largest Sum Subarray of Size at least K/largest-sum-subarray-of-size-at-least-k.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            long k = Long.parseLong(br.readLine().trim());
+            
+            Solution obj = new Solution();
+            System.out.println(obj.maxSumWithK(a, n, k));
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    
+    public long maxSumWithK(long a[], long n, long k)
+    {
+          long sum = 0;
+        for(int i = 0; i < k; i++){
+            sum += a[i];
+        }
+        long maxsum = sum;
+        long p = maxsum; 
+        for(int i = (int)k; i < n; i++){
+            sum = sum + a[i] - a[i - (int)k];
+            p = Math.max(p + a[i], sum);
+            maxsum = Math.max(maxsum, p);
+        }
+        return maxsum;
+    }
+}
