c96f9e71fc0ad3ac0d9752fe2a900f77c758783e
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-01-12 17:16:12 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse First K elements of Queue/reverse-first-k-elements-of-queue.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+class ModifyQueue {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking total number of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // Taking count of total number of elements
+            int n = sc.nextInt();
+
+            // Taking count of total elements
+            // that need to be reversed
+            int k = sc.nextInt();
+
+            // Creating a Queue
+            Queue<Integer> q = new LinkedList<>();
+
+            // adding all the elements to the Queue
+            while (n-- > 0) {
+                q.add((int)sc.nextInt());
+            }
+
+            // Creating an object of class GfG
+            GfG g = new GfG();
+
+            // calling modifyQueue of class GfG
+            // and passing Queue and k as arguments
+            // and storing the reuslt in a new Queue
+            Queue<Integer> ans = g.modifyQueue(q, k);
+
+            // Printing all the elements from the
+            // new Queue and polling them out
+            while (!ans.isEmpty()) {
+                int a = ans.peek();
+                ans.poll();
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class GfG {
+    // Function to reverse first k elements of a queue.
+    public Queue<Integer> modifyQueue(Queue<Integer> q, int k) {
+        // add code here.
+        Queue<Integer>q1=new LinkedList<>();
+        Stack<Integer>stack=new Stack();
+        int cnt=0;
+        while(cnt<k){
+            stack.push(q.remove());
+            cnt++;
+        }
+        while(!q.isEmpty()){
+            q1.add(q.remove());
+        }
+        while(!stack.isEmpty()){
+            q.add(stack.pop());
+        }
+         while(!q1.isEmpty()){
+            q.add(q1.remove());
+        }
+        
+       
+        return q;
+    }
+}
