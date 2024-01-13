511245da5804501b09efbb480c9dfa61f100cc25
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-01-13 17:05:56 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Insertion Sort for Singly Linked List/insertion-sort-for-singly-linked-list.java
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class insertion
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			insertion llist = new insertion(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		head = ob.insertionSort(llist.head);
+		llist.printList(head);
+		
+        t--;		
+        }
+    }}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    */
+class Solution
+{
+    public static Node insertionSort(Node head)
+    {
+        //code here
+        if(head == null || head.next == null)return head;
+        
+        Node t = head;
+       Node cur = head.next;
+        while(cur != null)
+        {
+            while(cur != t)
+            {
+                if(t.data > cur.data)
+                {
+                    int rahee = t.data;
+                    t.data = cur.data;
+                    cur.data = rahee;
+                }
+                t = t.next;
+                
+            }
+            t = head;
+            cur = cur.next;
+        }
+        return t;
+    }
+}
