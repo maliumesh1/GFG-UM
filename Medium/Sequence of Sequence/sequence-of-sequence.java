75943745bd3b8b7098d925b9c728d3bd9c8dccf4
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-01-16 13:45:28 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sequence of Sequence/sequence-of-sequence.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int m = Integer.parseInt(input_line[0]);
+            int n = Integer.parseInt(input_line[1]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.numberSequence(m, n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    int numberSequence(int m, int n){
+        if(m<n){
+            return 0;
+        }
+        if(n==0){
+            return 1;
+        }
+        return numberSequence(m/2,n-1)+numberSequence(m-1,n);
+    }
+}
