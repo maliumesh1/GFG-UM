504ba5307d1b641e0bca5779efb1dcc0385a6a54
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-03-05 09:31:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Index/maximum-index.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim()); // size of array
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]); // input elements of array
+		    }
+		    
+		    Solution ob = new Solution();
+		    
+		    System.out.println(ob.maxIndexDiff(arr, n)); // print the result
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    static int maxIndexDiff(int a[], int n) { 
+        
+        // Your code here
+                int st = 0, end = n-1;
+        int diff = 0;
+        while(st<=end){
+            if(a[st]<=a[end]){
+                diff = Math.max(diff,end-st);
+                st++;
+                end=n-1;
+            }else{
+                end--;
+            }
+        }
+        return diff;
+    }
+}
+
+
