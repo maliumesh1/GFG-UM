d3e72152e3ddbadc137deb9dec9862eb7df32143
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-03-11 16:01:24 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String input[] = read.readLine().split(" ");
+            
+            int n = Integer.parseInt(input[0]);
+            int x = Integer.parseInt(input[1]);
+            
+            int mat1[][] = new int[n][n];
+            
+            for(int i = 0;i<n;i++){
+                input = read.readLine().split(" ");
+                for(int j = 0;j<n;j++){
+                    mat1[i][j] = Integer.parseInt(input[j]);
+                }
+            }
+            
+            int mat2[][] = new int[n][n];
+            
+            for(int i = 0;i<n;i++){
+                input = read.readLine().split(" ");
+                for(int j = 0;j<n;j++){
+                    mat2[i][j] = Integer.parseInt(input[j]);
+                }
+            }
+            
+        
+            
+            Solution ob = new Solution();
+            System.out.println(ob.countPairs(mat1,mat2,n,x));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int countPairs(int mat1[][], int mat2[][], int n, int x) {
+        // code here
+         int res = 0;
+        for(int[] row : mat1){
+            for(int num : row){
+                int target = x-num;
+                res += search(mat2, target);
+            }
+        }
+        return res;
+    }
+    
+    public int search(int[][] mat , int target){
+        int n = mat.length;
+        int r=0, c=n-1;
+        while(r<n && c>=0){
+            if(mat[r][c] == target) return 1;
+            if(mat[r][c] < target) r++;
+            else c--;
+        }
+        return 0;
+    }
+}
