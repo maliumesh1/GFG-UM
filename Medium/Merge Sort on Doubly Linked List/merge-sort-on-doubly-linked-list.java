c4a0b27851ba90d94798b412cd8513e77f1ad1e6
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-04-28 08:55:49 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge Sort on Doubly Linked List/merge-sort-on-doubly-linked-list.java
@@ -0,0 +1,133 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next, prev;
+
+    Node(int key) {
+        data = key;
+        next = prev = null;
+    }
+}
+
+class Driverclass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node temp = head;
+
+            for (int i = 1; i < n; i++) {
+                int a = sc.nextInt();
+                Node n1 = new Node(a);
+                n1.prev = temp;
+                temp.next = n1;
+                temp = n1;
+            }
+
+            head = new Solution().sortDoubly(head);
+            printList(head);
+        }
+    }
+
+    public static void printList(Node node) {
+        Node temp = node;
+        while (node != null) {
+            System.out.print(node.data + " ");
+            temp = node;
+            node = node.next;
+        }
+        System.out.println();
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.prev;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next, prev;
+    Node(int data)
+    {
+        this.data = data;
+        next = prev = null;
+    }
+}
+*/
+class Solution {
+    // Function to sort the given doubly linked list using Merge Sort.
+    static Node sortDoubly(Node head) {
+        // add your code here
+          if (head == null || head.next == null) {
+            return head;
+        }
+        
+        // Find the middle of the list
+        Node middle = getMiddle(head);
+        Node nextOfMiddle = middle.next;
+        middle.next = null;
+        nextOfMiddle.prev = null;
+        
+        // Recursively sort both halves
+        Node left = sortDoubly(head);
+        Node right = sortDoubly(nextOfMiddle);
+        
+        // Merge the sorted halves
+        return merge(left, right);
+    }
+    
+    // Function to find the middle node of the doubly linked list
+    static Node getMiddle(Node head) {
+        if (head == null) {
+            return head;
+        }
+        
+        Node slow = head;
+        Node fast = head.next;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+    
+    // Function to merge two sorted doubly linked lists
+    static Node merge(Node left, Node right) {
+        if (left == null) {
+            return right;
+        }
+        
+        if (right == null) {
+            return left;
+        }
+        
+        if (left.data <= right.data) {
+            left.next = merge(left.next, right);
+            left.next.prev = left;
+            left.prev = null;
+            return left;
+        } else {
+            right.next = merge(left, right.next);
+            right.next.prev = right;
+            right.prev = null;
+            return right;
+        }
+    }
+}
