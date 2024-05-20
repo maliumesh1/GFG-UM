828798c906d2d71eae3827b6d8a8d1faf4159bb5
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-05-20 13:51:40 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Modular Exponentiation for large numbers/modular-exponentiation-for-large-numbers.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            long x = Long.parseLong(S[0]);
+            long n = Long.parseLong(S[1]);
+            long m = Long.parseLong(S[2]);
+            Solution ob = new Solution();
+            long ans = ob.PowMod(x, n, m);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long PowMod(long x, long n, long m)
+    {
+        // Code here
+        long rahee = 1;
+        if(n == 0) return 1;
+        if(n ==1) return x;
+         rahee = PowMod(x,(n/2),m);
+         rahee = (rahee  *rahee)%m;
+         if(n%2 ==1)
+         {
+             return (rahee *x)%m;
+         }
+         return rahee ;
+    }
+}
