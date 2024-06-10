a46696c7dc3560cc007bbbdc78b72f5c55881d04
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-06-10 15:42:21 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Nuts and Bolts Problem/nuts-and-bolts-problem.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            char[] nuts = new char[n], bolts = new char[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                nuts[i] = (inputLine[i].charAt(0));
+            }
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                bolts[i] = (inputLine[i].charAt(0));
+            }
+
+            new Solution().matchPairs(n, nuts, bolts);
+            for (int i = 0; i < n; i++) {
+                System.out.print(nuts[i] + " ");
+            }
+            System.out.println();
+            for (int i = 0; i < n; i++) {
+                System.out.print(bolts[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        // code here
+         int hash[] = new int [256];
+        for(int i = 0;i < n;i++){
+            hash[nuts[i]]++;
+        }
+        
+        int j = 0;
+        for(int i = 0;i < 256;i++){
+            while(hash[i] != 0){
+                nuts[j] = (char)i;
+                hash[i]--;
+                j++;
+            }
+        }
+        for(int i = 0;i < n;i++){
+            bolts[i] = nuts[i];
+        }
+        
+    }
+}
