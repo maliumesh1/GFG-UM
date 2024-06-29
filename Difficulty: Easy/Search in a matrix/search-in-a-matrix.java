ecb22b6cf858105813bf28b1e42df819b3c6fc96
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-06-29 22:36:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in a matrix/search-in-a-matrix.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            
+            int mat[][] = new int[n][m];
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                  mat[i][j] = sc.nextInt();
+            }
+            
+            int x = sc.nextInt();
+            
+            System.out.println (new Sol().matSearch(mat, n, m, x));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+class Sol
+{
+    public static int matSearch(int mat[][], int N, int M, int X)
+    {
+        // your code here
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(mat[i][j]==X){
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+}
