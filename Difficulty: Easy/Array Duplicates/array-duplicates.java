74405244d38ccfb36edf6272eeebaa4e0e539258
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-06-30 00:27:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.duplicates(arr);
+
+            IntArray.print(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static ArrayList<Integer> duplicates(int[] arr) {
+        // code here
+        boolean flag = false;
+        ArrayList<Integer> answer = new ArrayList<Integer>(arr.length);
+        for(int i = 0; i < arr.length; i++){
+            int curr = arr[i];
+            for(int j = i + 1; j < arr.length; j++){
+                if(curr == arr[j]){
+                    flag = true;
+                    answer.add(curr);
+                }
+            }
+        }
+        if(!flag){
+            answer.add(-1);
+        }
+        return answer;
+    }
+}
+
