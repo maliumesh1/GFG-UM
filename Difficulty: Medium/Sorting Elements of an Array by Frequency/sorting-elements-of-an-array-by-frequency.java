7acab93ec2db0380bde28356a3d3573f859fb4d4
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-08-28 14:59:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sorting Elements of an Array by Frequency/sorting-elements-of-an-array-by-frequency.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    // Function to sort the array according to frequency of elements.
+    public ArrayList<Integer> sortByFreq(int arr[]) {
+        // add your code here
+        HashMap<Integer,Integer> map= new HashMap<>();
+        ArrayList<Integer> ans= new ArrayList<>();
+        
+        for(int val:arr){
+             ans.add(val);
+             map.put(val,map.getOrDefault(val,0)+1);
+        }
+
+        Collections.sort(ans,(a,b)->{
+            int fa= map.get(a);
+            int fb= map.get(b);
+            if(fa==fb) return a-b;
+            else return fb-fa;
+        });
+        
+        return ans;
+    }
+}
+
+
+
+//{ Driver Code Starts.
+
+class Driverclass {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(sc.readLine());
+        while (n != 0) {
+            String input = sc.readLine();
+            String[] inputs = input.split(" ");
+            int[] arr = new int[inputs.length];
+
+            for (int i = 0; i < inputs.length; i++) {
+                arr[i] = Integer.parseInt(inputs[i]);
+            }
+
+            ArrayList<Integer> ans = new ArrayList<Integer>();
+            ans = new Solution().sortByFreq(arr);
+            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
+            System.out.println();
+            n--;
+        }
+    }
+}
+
+// } Driver Code Ends
