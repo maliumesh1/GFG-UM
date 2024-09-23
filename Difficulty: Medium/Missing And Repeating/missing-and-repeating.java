4d81a6ed6fdd719b429c5e7d1d4b105b97c39688
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-09-23 19:34:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Missing And Repeating/missing-and-repeating.java
+++ Difficulty: Medium/Missing And Repeating/missing-and-repeating.java
@@ -7,8 +7,7 @@ import java.util.*;
 class GFG {
     // Driver code
     public static void main(String[] args) throws Exception {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine().trim());
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine().trim());
@@ -19,7 +18,7 @@ class GFG {
                 a[i] = Integer.parseInt(str[i]);
             }
 
-            int[] ans = new Solve().findTwoElement(a, n);
+            int[] ans = new Solve().findTwoElement(a);
             System.out.println(ans[0] + " " + ans[1]);
         }
     }
@@ -28,22 +27,25 @@ class GFG {
 
 
 // User function Template for Java
+
 class Solve {
-    int[] findTwoElement(int arr[], int n) {
-        // code here
-        int result[]=new int [2];
-        int count[]=new int [n+1];
-        for(int i=0;i<n;i++){
-           count[arr[i]]++; 
-        }
-        for(int i=1;i<=n;i++){
-            if(count[i]==2){
-                result[0]=i;
-            }
-            else if(count[i]==0){
-                result[1]=i;
-            }
-        }
-        return result;
+    int[] findTwoElement(int arr[]) {
+        int res[]=new int[2];
+        int n=arr.length;
+       int tmp[]=new int[arr.length+1];
+       for(int i=0;i<n;i++)
+       tmp[arr[i]]++;
+       int x=0,y=0;
+       for(int i=1;i<tmp.length;i++)
+       {
+           if(tmp[i]==0)
+           x=i;
+           else if(tmp[i]>1)
+           y=i;
+       }
+       res[0]=y;
+      res[1]=x;
+        return res;
     }
 }
+
