28d415e1be68e35f72e976a1bc4aab6839927820
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-10-05 23:53:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Not a subset sum/not-a-subset-sum.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+        while (t-- > 0) {
+            String input = sc.nextLine();
+            String[] inputArr = input.split(" ");
+            int[] arr = new int[inputArr.length];
+            for (int i = 0; i < inputArr.length; i++) {
+                arr[i] = Integer.parseInt(inputArr[i]);
+            }
+
+            Solution ob = new Solution();
+            long ans = ob.findSmallest(arr);
+            System.out.println(ans);
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    public long findSmallest(int[] arr) {
+        long smallestMissing = 1;
+        
+        for(int num : arr){
+            if(num > smallestMissing){
+                break;
+            }
+            
+            smallestMissing += num;
+        }
+        
+        return smallestMissing;
+    }
+}
