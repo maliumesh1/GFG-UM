c7ed9b3f795311848fa34f94d1a9e4fa852ae35d
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-12-06 16:50:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int test_cases = Integer.parseInt(br.readLine().trim());
+
+        while (test_cases-- > 0) {
+            // Read the array from input line
+            String[] input = br.readLine().trim().split(" ");
+            int[] arr = new int[input.length];
+            for (int i = 0; i < input.length; i++) {
+                arr[i] = Integer.parseInt(input[i]);
+            }
+
+            // Solution instance to invoke the function
+            Solution ob = new Solution();
+            int result = ob.hIndex(arr);
+
+            System.out.println(result);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    // Function to find hIndex
+  public int hIndex(int[] citations) {
+        // code here
+        int count=0;
+        
+        int n = citations.length;
+        for(int i=n;i>0;i--){
+            // if(citations[i]==0);
+            int current = i;
+            for(int j=0;j<n;j++){
+                if(citations[j]>=current){
+                    count++;
+                }
+            }
+            if(count == current){
+                return count;
+            }
+            else if(count >= current){
+                return current;
+            }
+            else{
+                count=0;
+            }
+        }
+        return count;
+    }
+
+
+}
