e8ce1449d8d6c88931e65513cc37a2b24cba05fd
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-12-11 22:03:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Merge Without Extra Space/merge-without-extra-space.java
+++ Difficulty: Hard/Merge Without Extra Space/merge-without-extra-space.java
@@ -1,68 +1,61 @@
 //{ Driver Code Starts
-//Initial Template for Java
-
+import java.io.*;
 import java.util.*;
-import java.io.*;
-import java.io.*;
 
-public class Main
-{
-    public static void main (String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
-		while(t-->0){
-		    String inputLine[] = br.readLine().trim().split(" ");
-		    int n = Integer.parseInt(inputLine[0]);
-		    int m = Integer.parseInt(inputLine[1]);
-		    long arr1[] = new long[n];
-		    long arr2[] = new long[m];
-		    inputLine = br.readLine().trim().split(" ");
-		    for(int i=0; i<n; i++){
-		        arr1[i] = Long.parseLong(inputLine[i]);
-		    }
-		    inputLine = br.readLine().trim().split(" ");
-		    for(int i=0; i<m; i++){
-		        arr2[i] = Long.parseLong(inputLine[i]);
-		    }
-		    Solution ob = new Solution();
-		    ob.merge(arr1, arr2, n, m);
-		    
-		    StringBuffer str = new StringBuffer();
-		    for(int i=0; i<n; i++){
-		        str.append(arr1[i]+" ");
-		    }
-		    for(int i=0; i<m; i++){
-		        str.append(arr2[i]+" ");
-		    }
-		    System.out.println(str);
-		}
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+
+        while (t-- > 0) {
+            String arr1[] = br.readLine().trim().split(" ");
+            String arr2[] = br.readLine().trim().split(" ");
+
+            int a[] = new int[arr1.length];
+            int b[] = new int[arr2.length];
+
+            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(arr1[i]);
+            for (int i = 0; i < b.length; i++) b[i] = Integer.parseInt(arr2[i]);
+
+            Solution ob = new Solution();
+            ob.mergeArrays(a, b);
+
+            StringBuffer str = new StringBuffer();
+            for (int i = 0; i < a.length; i++) {
+                str.append(a[i] + " ");
+            }
+            System.out.println(str);
+            str = new StringBuffer();
+            for (int i = 0; i < b.length; i++) {
+                str.append(b[i] + " ");
+            }
+            System.out.println(str);
+        }
+    }
 }
 
 // } Driver Code Ends
 
+class Solution {
+    public void mergeArrays(int[] a, int[] b) {
+        int m = a.length, n = b.length;
 
-//User function Template for Java
+        // Traverse both arrays and make sure 'a' always has the smallest elements in sorted order.
+        for (int i = 0; i < m; i++) {
+            if (a[i] > b[0]) {
+                // Swap the larger element from 'a' with the smallest element in 'b'.
+                int temp = a[i];
+                a[i] = b[0];
+                b[0] = temp;
 
-class Solution
-{
-   public static void merge(long arr1[], long arr2[], int n, int m) 
-    {
-        int p1 = n-1;
-        int p2 = 0;
-        
-        while(p1 >= 0 && p2 < m){
-            if(arr1[p1] >= arr2[p2]){
-                long temp = arr1[p1];
-                arr1[p1] = arr2[p2];
-                arr2[p2] = temp;
-                p1--;
-                p2++;
-            } else if (arr1[p1] < arr2[p2]){
-                break;
+                // Maintain 'b' as sorted.
+                int first = b[0];
+                int k;
+                for (k = 1; k < n && b[k] < first; k++) {
+                    b[k - 1] = b[k];
+                }
+                b[k - 1] = first;
             }
         }
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
     }
 }
