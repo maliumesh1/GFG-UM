9335c1b16ec727ad41f55836bb3c35e6e7dad969
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2024-12-20 22:47:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.java
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.java
@@ -20,55 +20,41 @@ class GFG {
             ArrayList<Integer> ans = ob.spirallyTraverse(matrix);
             for (Integer val : ans) System.out.print(val + " ");
             System.out.println();
+
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
-
-class Solution
-{
-    //Function to return a list of integers denoting spiral traversal of matrix.
-    static ArrayList<Integer> spirallyTraverse(int arr[][])
-    {
-        // code here 
-         ArrayList<Integer>list=new ArrayList<>();
-        int count1=0;
-        int row=arr.length;
-        int column=arr[0].length;
-        int a=0,n=column,p=0;
-        int b=1,m=row-1,q=column-1;
-        int c=column-1,o=0,r=row-1;
-        int d=row-2,s=1,t=0;
-        outer:while(true)
-        {
-
-            for(int i=a;i<n;i++){
-                list.add(arr[p][i]);++count1;
-                if(count1==row*column)
-                    return list;
-            }p++;a++;n--;
-
-            for(int i=b;i<m;i++){
-                 list.add(arr[i][q]);
-                ++count1;
-                if(count1==row*column)
-                    return list;
-            }q--;b++;m--;
-
-            for(int i=c;i>=o;i--){
-                 list.add(arr[r][i]);
-                ++count1;
-                if(count1==row*column)
-                    return list;
-            }r--;c--;o++;
-
-            for(int i=d;i>=s;i--){
-                 list.add(arr[i][t]);
-                ++count1;
-                if(count1==row*column)
-                    return list;
-            }t++;d--;s++;
+class Solution {
+    // Function to return a list of integers denoting spiral traversal of matrix.
+    public ArrayList<Integer> spirallyTraverse(int mat[][]) {
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+        int n = mat.length;
+        int m = mat[0].length;
+        int s = n*m;
+        int a = 0, b = 0;
+        int c = n-1, d = m-1;
+        
+        if(n == 0 || m == 0) return arr;
+        
+        while(arr.size() < s) {
+            
+            for(int i=b; i<=d && arr.size()<s; i++) arr.add(mat[a][i]);
+            a++;
+            
+            for(int i=a; i<=c && arr.size()<s; i++) arr.add(mat[i][d]);
+            d--;
+            
+            for(int i=d; i>=b && arr.size()<s; i--) arr.add(mat[c][i]);
+            c--;
+            
+            for(int i=c; i>=a && arr.size()<s; i--) arr.add(mat[i][b]);
+            b++;
         }
+        
+        return arr;
     }
 }
