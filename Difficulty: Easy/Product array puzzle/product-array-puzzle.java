e938442ceac536516fabe2b4365e4b402ad6fbfb
Umesh Mali <111166993+maliumesh1@users.noreply.github.com>
2025-01-17 18:08:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.java
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.java
@@ -6,22 +6,24 @@ import java.util.*;
 
 class GFG {
     public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
         while (t > 0) {
-            int n = sc.nextInt();
-            int[] array = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
             for (int i = 0; i < n; i++) {
-                array[i] = sc.nextInt();
+                arr[i] = Integer.parseInt(inputLine[i]);
             }
             Solution ob = new Solution();
-            long[] ans = new long[n];
-            ans = ob.productExceptSelf(array);
+            int[] ans = new int[n];
+            ans = ob.productExceptSelf(arr);
 
             for (int i = 0; i < n; i++) {
                 System.out.print(ans[i] + " ");
             }
             System.out.println();
+            System.out.println("~");
             t--;
         }
     }
@@ -29,42 +31,24 @@ class GFG {
 
 // } Driver Code Ends
 
-
-// User function Template for Java
-
 class Solution {
-    public static long[] productExceptSelf(int nums[]) {
+    public static int[] productExceptSelf(int arr[]) {
         // code here
-        long arr[]=new long[nums.length];
-        long res=1;
-        boolean t=true;
-        int count=0;;
-        for(int i=0;i<nums.length;i++){
-           if(nums[i]!=0){
-                res*=nums[i];
-           }else{
-               t=false;
-               count++;
-           }
-           
+        int n=arr.length;
+        int left[]=new int[n];
+        int right[]=new int[n];
+        int res[]=new int[n];
+        left[0]=arr[0];
+        right[n-1]=arr[n-1];
+        for(int i=1;i<n-1;i++){
+            left[i]=left[i-1]*arr[i];
+            right[n-i-1]=right[n-i]*arr[n-i-1];
         }
-        if(t){
-            for(int i=0;i<nums.length;i++){
-                arr[i]=res/nums[i];
-            }
-        }else{
-            if(count==1){
-                for(int i=0;i<nums.length;i++){
-                    if(nums[i]==0){
-                        arr[i]=res;
-                    }else{
-                        arr[i]=0;
-                    }
-                }
-            }else{
-                Arrays.fill(arr,0);
-            }
+        res[0]=right[1];
+        res[n-1]=left[n-2];
+        for(int i=1;i<n-1;i++){
+            res[i]=left[i-1]*right[i+1];
         }
-        return arr;
+        return res;
     }
 }
